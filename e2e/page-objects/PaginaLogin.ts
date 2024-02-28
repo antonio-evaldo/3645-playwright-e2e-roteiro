d16cfc5c1a1ffb5f14d203160139465477fbaa04
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-28 18:23:46 -0300

    Vídeo 3.3 - Interagindo com Locators

--- e2e/page-objects/PaginaLogin.ts
+++ e2e/page-objects/PaginaLogin.ts
@@ -1,13 +1,17 @@
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export default class PaginaLogin {
   readonly page: Page;
+  readonly botaoLogin: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.botaoLogin = page.getByTestId('botao-login');
   }
 
   async visitar() {
     await this.page.goto('/');
+    await this.botaoLogin.click();
+    await expect(this.page).toHaveURL('/auth/login');
   }
 }
