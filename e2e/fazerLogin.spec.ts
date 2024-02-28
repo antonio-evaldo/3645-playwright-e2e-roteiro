fb96c9f4d863633b2adbe69ed2efb6a21e04ac85
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-28 17:28:44 -0300

    Vídeo 3.2 - Page Object Model

---
+++ e2e/fazerLogin.spec.ts
@@ -0,0 +1,10 @@
+import test from "@playwright/test";
+import PaginaLogin from "./page-objects/PaginaLogin";
+
+test.describe('Página de Login', () => {
+  test('Deve conseguir fazer login com email e senha válidos', async ({ page }) => {
+    const paginaLogin = new PaginaLogin(page);
+
+    await paginaLogin.visitar();
+  });
+});
